194bf6fbd09382add706ca0f5d47af19b1185fb9
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2024-01-25 23:23:35 +0200

    establish architecture

---
+++ AlbumPhotos/Service/Network.swift
@@ -0,0 +1,8 @@
+//
+//  Network.swift
+//  AlbumPhotos
+//
+//  Created by Alaa on 25/01/2024.
+//
+
+import Foundation
---
+++ AlbumPhotos/Service/NetworkProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkProtocol.swift
+//  AlbumPhotos
+//
+//  Created by Alaa on 25/01/2024.
+//
+
+import Foundation
+
+protocol NetworkProtocol {
+    
+    
+}
